0ea7a552276d1b6f33205103633749fff606f4c1
Kern <kding@whpm.com>
2014-04-16 07:57:44 -0800

    hw2 start

---
+++ spell/java/SpellCorrector.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SpellCorrector {
+  // TODO(student): Feel free define private variables here to store whatever
+  // you trained.
+
+  SpellCorrector() {
+    // TODO(student): Contructor to initialize your data-structure
+  }
+
+  public void train(final String word) {
+    // TODO(student): Implement your own logic to model spell corrector using
+    // edit distance. This method will be called for every possible known valid
+    // word from corpus. Learn your model from this.
+  }
+
+  public String correct(final String mispelled_word) {
+    // TODO(student): Use the model learnt in above method and return correct
+    // spelling for given word. Given a mispelled word, return your best
+    // predicted corrected word.
+    return wrong_word;
+  }
+
+  // TODO(student): Feel free to define private utility methods here.
+}
